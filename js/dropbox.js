f9b13b390644ba4b592165b61d8ad83801f5e751
YouthsSpy <YouthsSpy@gmail.com>
2015-05-29 12:45:50 +0300

    Update and rename dropbox.js to drive.js

--- js/dropbox.js
+++
@@ -1,7 +0,0 @@
-
-var client = new Dropbox.Client({ key: '53zuip3cnv6k1c7' });
-client.authenticate(function () {
-    client.writeFile('data.txt', 'Hello, World!', function () {
-        alert('File written!');
-    });
-});
