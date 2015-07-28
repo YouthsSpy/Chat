1d5678ce818e95634a079fc1136e6c65c1b7ac28
YouthsSpy <YouthsSpy@gmail.com>
2015-07-28 13:40:10 +0300

    Update message.js

--- js/message.js
+++ js/message.js
@@ -1,7 +1,7 @@
-
+var date = "28-07-2015";
 var text = "אני לא יכול יותר לדבר איתך... מצטער... זה בשבילך...";
 
 function ShowMyMessageTest(){
   var pass = prompt("");
-  if (pass.toLowerCase() == "youthsspy") alert(text);
+  if (pass.toLowerCase() == "youthsspy") alert(date + "\r\n\r\n" + text);
 }
